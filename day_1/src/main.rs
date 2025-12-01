1a2ad56e63fad130401bcf8a9b9b8ea205131730
Guillaume Galy <guilgaly@gmail.com>
2025-12-01 13:45:18 +0100

    Day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,33 +1,88 @@
-use common::itertools::Itertools;
-use common::sscanf::sscanf;
 use std::error::Error;
 
 static INPUT: &str = include_str!("input");
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // e.g. read lines
-    let lines = INPUT.lines().collect_vec();
-    // e.g. parse lines with sscanf
-    let _parsed_lines = INPUT
-        .lines()
-        .map(|line| sscanf!(line, "{usize},{str}"))
-        .collect::<Result<Vec<_>, _>>()?;
+    let rotations = parse_rotations(&INPUT)?;
 
-    let res1 = part_1(&lines);
+    let res1 = part_1(&rotations);
     println!("Part 1 result: {}", res1);
 
-    let res2 = part_2(&lines);
+    let res2 = part_2(&rotations);
     println!("Part 2 result: {}", res2);
 
     Ok(())
 }
 
-fn part_1(lines: &[&str]) -> usize {
-    lines.len()
+fn part_1(rotations: &[i64]) -> u64 {
+    let mut password = 0;
+    let mut position = 50;
+
+    for rotation in rotations {
+        let effective_rotation = rotation % 100;
+        let raw_position = position + effective_rotation;
+        position = if raw_position < 0 {
+            100 + raw_position
+        } else if raw_position > 99 {
+            raw_position - 100
+        } else {
+            raw_position
+        };
+        if position == 0 {
+            password += 1;
+        }
+    }
+
+    password
 }
 
-fn part_2(lines: &[&str]) -> usize {
-    lines.len()
+fn part_2(rotations: &[i64]) -> u64 {
+    let mut password = 0;
+    let mut position = 50;
+
+    for rotation in rotations {
+        // Count all full rotations first, since they simply bring us back to the same position
+        password += (rotation / 100).unsigned_abs();
+
+        // Apply remaining clicks
+        let starting_position = position;
+        let effective_rotation = rotation % 100;
+        if effective_rotation != 0 {
+            let raw_position = starting_position + effective_rotation;
+            position = if raw_position < 0 {
+                100 + raw_position
+            } else if raw_position > 99 {
+                raw_position - 100
+            } else {
+                raw_position
+            };
+
+            // Count if the remaining clicks included 0. There is one subtle case: do not count when
+            // going left _from zero_ (that zero was already counted on the previous rotation)
+            if (starting_position != 0 && raw_position <= 0) || raw_position >= 100 {
+                password += 1;
+            }
+        }
+    }
+
+    password
+}
+
+fn parse_rotations(input: &str) -> Result<Vec<i64>, Box<dyn Error>> {
+    input
+        .lines()
+        .map(|line| {
+            let (first, last) = line.split_at(1);
+            let count: i64 = last.parse()?;
+            if first == "R" {
+                Ok(count)
+            } else if first == "L" {
+                Ok(-count)
+            } else {
+                Err("Invalid rotation".into())
+            }
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -36,13 +91,13 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        let test_input = ["A", "B"];
-        assert_eq!(part_1(&test_input), 2);
+        let test_input = [-68, -30, 48, -5, 60, -55, -1, -99, 14, -82];
+        assert_eq!(part_1(&test_input), 3);
     }
 
     #[test]
     fn test_part_2() {
-        let test_input = ["A", "B"];
-        assert_eq!(part_2(&test_input), 2);
+        let test_input = [-68, -30, 48, -5, 60, -55, -1, -99, 14, -82];
+        assert_eq!(part_2(&test_input), 6);
     }
 }
